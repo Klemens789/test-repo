ede922c952024c2cbb44774ea105b6e2519e6c22
Klemens789 <159935734+Klemens789@users.noreply.github.com>
2024-03-26 16:26:44 +0100

    Delete klemens directory

--- klemens/form.php
+++
@@ -1,8 +0,0 @@
-
-<form action="wynik.php" method="POST">
-    Podaj Imie:
-    <input type="text" name="imie">
-    Podaj nazwisko: 
-    <input type="text" name="nazwisko" placeholder="Tutaj wypisz ...">
-    <input type="submit" value="Wyslij">
-</form>     
