2158175306a32bd1424a0a22ed20ef1160d34d28
Klemens789 <159935734+Klemens789@users.noreply.github.com>
2024-03-26 16:24:08 +0100

    Delete wynik.php

--- wynik.php
+++
@@ -1,9 +0,0 @@
-<?php
-echo "<h2 style=\"color:red\">Witaj " . $_POST["imie"] ." ". $_POST["nazwisko"] . "</h2><br>";
-echo  "Pełnoletni"." ".$_POST["wiek"];
-echo "<br>";
-foreach($_POST["hobby"] as $wartosc){
-    echo $wartosc;
-    echo "<br>";
-}
-?>
